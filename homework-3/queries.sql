ce4fa91d0f76af52e7b29e2b894b58cc6c24bdf8
SergeyOlkin <4567310Ajiger@gmail.com>
2023-07-17 17:49:00 +0300

    Added changes

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT cust.company_name, emp.first_name || ' ' || emp.last_name as employee_name
+SELECT cust.company_name, CONCAT(emp.first_name, ' ', emp.last_name) as employee_name
 FROM customers AS cust
 JOIN orders AS ord ON cust.customer_id = ord.customer_id
 JOIN employees AS emp ON ord.employee_id = emp.employee_id
